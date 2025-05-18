79b99e3e97746086b2aab23cb2e3fc847117ed74
Louis <louis@MacBook-Pro-de-Louis.local>
2025-05-19 00:36:58 +0200

    Fixing favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
         <head>
             <title>Professional Portfolio | GitHub Projects Showcase</title>
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <link rel="icon" href="/favicon.ico" />
+            <link rel="icon" href="/pics/favicon.ico" />
             <link
                 rel="stylesheet"
                 href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
